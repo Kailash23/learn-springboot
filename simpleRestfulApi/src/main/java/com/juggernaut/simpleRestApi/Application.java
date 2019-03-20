b2e5c2afef2ec7a8b9e61c26f8cd83632066d0e3
Kailash23 <kailashuniyal04@gmail.com>
2019-03-20 12:26:50 +0530

    first commit

---
+++ simpleRestfulApi/src/main/java/com/juggernaut/simpleRestApi/Application.java
@@ -0,0 +1,13 @@
+package com.juggernaut.simpleRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
