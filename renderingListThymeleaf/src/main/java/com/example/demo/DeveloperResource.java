411f7659fffe9eb6a1b3b5d96646669a85414c67
Kailash23 <kailashuniyal04@gmail.com>
2019-03-25 15:18:20 +0530

    thymeleaf demos

---
+++ renderingListThymeleaf/src/main/java/com/example/demo/DeveloperResource.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class DeveloperResource {
+	private String name;
+	private String url;
+
+	public DeveloperResource(String name, String url) {
+		this.name = name;
+		this.url = url;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+}
